2afb439e390a19c65cdc82932a2943e379ba7bf3
JASS BOTS <157136102+jassbots@users.noreply.github.com>
2024-05-12 10:25:35 +0530

    Update afk.py

--- AnonXMusic/plugins/tools/afk.py
+++ AnonXMusic/plugins/tools/afk.py
@@ -3,7 +3,6 @@ from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
 from AnonXMusic import app
-from AnonXMusic.mongo.readable_time import get_readable_time
 from AnonXMusic.mongo.afkdb import add_afk, is_afk, remove_afk
 
 
